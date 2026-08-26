a3655a957b609bfd7e7294c16617a5714fcd81dc
zurabsamkharadze-png <zurabsamkharadze@gmail.com>
2026-08-26 14:29:05 +0400

    Serve valuation history client script

---
+++ api/valuation-history-client.js
@@ -0,0 +1,16 @@
+const fs=require('fs');
+const path=require('path');
+module.exports=(req,res)=>{
+  try{
+    const file=path.join(process.cwd(),'public','valuation-history-client.js');
+    const js=fs.readFileSync(file,'utf8');
+    res.statusCode=200;
+    res.setHeader('content-type','application/javascript; charset=utf-8');
+    res.setHeader('cache-control','no-store, max-age=0, must-revalidate');
+    res.end(js);
+  }catch(e){
+    res.statusCode=500;
+    res.setHeader('content-type','application/javascript; charset=utf-8');
+    res.end("console.error('valuation history client unavailable')");
+  }
+};
