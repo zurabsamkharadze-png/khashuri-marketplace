0fbd234e5f46bd4a6b01581bd4a4fe1cac572559
zurabsamkharadze-png <zurabsamkharadze@gmail.com>
2026-08-27 06:52:00 +0400

    Normalize stale outbuildings before repeat valuation

--- api/valuation-history-client.js
+++ api/valuation-history-client.js
@@ -8,7 +8,8 @@ module.exports=(req,res)=>{
     const file=path.join(process.cwd(),'public',name);
     let js=fs.readFileSync(file,'utf8');
     if(actions){
-      js=js.replace("location.href='/valuation/new?mode=precise&repeat=1'","location.href='/valuation/new?mode=precise&repeat=1&v=14'");
+      js=js.replace("location.href='/valuation/new?mode=precise&repeat=1'","location.href='/valuation/new?mode=precise&repeat=1&v=17'");
+      js=js.replace('function saveRepeat(v){','function saveRepeat(v){if(Number(v?.calculation_details?.outbuilding_component||0)===0){v={...v,outbuildings:[],input_data:{...(v.input_data||{}),outbuildings:[]}};}');
     }
     res.statusCode=200;
     res.setHeader('content-type','application/javascript; charset=utf-8');
